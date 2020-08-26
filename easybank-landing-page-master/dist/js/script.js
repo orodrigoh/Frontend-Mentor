f3cfc6e01906a659cc64cbe1489727ea4d24a392
Rodrigo Henrique <roodrigoooh@gmail.com>
2020-08-26 18:54:46 -0300

    Desktop done
    Desktop version is done.

--- easybank-landing-page-master/dist/js/script.js
+++ easybank-landing-page-master/dist/js/script.js
@@ -1,16 +1,7 @@
-const menuMobile = document.querySelector('.js-hamburger');
-const overlay = document.querySelector('.overlay');
-const links = document.querySelector('.js-links');
-function callback() {
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        overlay.classList.remove('open');
-        links.classList.remove('open');
+const menuMobile = document.querySelector('.header');
 
-    }else {
-        menuMobile.classList.add('open');
-        overlay.classList.add('open');
-        links.classList.add('open');
-    }
+
+function callback() {
+    menuMobile.classList.toggle('open');
 }
 menuMobile.addEventListener('click', callback);
